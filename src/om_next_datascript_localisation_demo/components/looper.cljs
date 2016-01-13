0643b6c430ea1452bbd7769676f5bd65d31f17d9
Simon Blanchard <simonb@yajogo.com>
2016-01-13 19:18:01 +0800

    remember to start/stop the interval timer for looper

--- src/om_next_datascript_localisation_demo/components/looper.cljs
+++ src/om_next_datascript_localisation_demo/components/looper.cljs
@@ -4,11 +4,32 @@
     [sablono.core :as html :refer-macros [html]]
     [om-next-datascript-localisation-demo.logging :refer-macros [log]]))
 
+(defn make-timer [this]
+  (js/setInterval #(.inc-index this) 4000))
 
 (defui Looper
   Object
   (initLocalState [this]
-    {:current-index 0})
+    {:current-index 0 :timer 0})
+
+  (componentWillMount [this]
+    ;;(log "Looper: componentWillMount")
+    (om/update-state! this assoc :timer (make-timer this)))
+
+  (componentWillUnmount [this]
+    ;;(log "Looper: componentWillUnmount")
+    (if-let [timer (om/get-state this :timer)]
+      (js/clearInterval timer)))
+
+  (componentWillReceiveProps [this new-props]
+    ;;(log "Looper: componentWillReceiveProps")
+    (if-let [timer (om/get-state this :timer)]
+      (js/clearInterval timer))
+    (let [things (:things new-props)
+          length (count things)]
+      (if (> length 0)
+        (om/set-state! this {:current-index 0 :timer (make-timer this)})
+        (om/set-state! this {:current-index 0 :timer 0}))))
 
   (inc-index [this]
     (let [props (om/props this)
@@ -17,7 +38,8 @@
           current-index (om/get-state this :current-index)
           new-index (inc current-index)
           new-index (if (>= new-index length) 0 new-index)]
-      (om/set-state! this {:current-index new-index})))
+      ;;(log "Looper: inc-index")
+      (om/update-state! this assoc :current-index new-index)))
 
   (render [this]
     (let [props (om/props this)
@@ -25,8 +47,6 @@
           current-index (om/get-state this :current-index)]
       ;;(log "Looper: render:" props)
       ;;(log "Looper: render: current-index" current-index)
-      (js/setTimeout #(.inc-index this) 4000)
       (html [:p (nth things current-index)]))))
-
 
 (def looper (om/factory Looper))
